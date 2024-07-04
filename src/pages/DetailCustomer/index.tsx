84bf8f8d18eeab56cfb50fba98bbf49ba8a11369
LrVanAnhDuc <levananhduc1804@gmail.com>
2024-07-04 11:08:09 +0700

    refactor code detail users

--- src/pages/DetailCustomer/index.tsx
+++ src/pages/DetailCustomer/index.tsx
@@ -1,14 +1,15 @@
+// libs
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import TextField from '@mui/material/TextField';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-
+import Avatar from '@mui/material/Avatar';
 import { Controller, useForm } from 'react-hook-form';
 import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-
-import config from '../../config';
-import IUser from '../../types/user';
-import Image from '../../components/Image';
+// types
+import IUser from '@/types/user';
+// others
+import config from '@/config';
 
 const DetailCustomer = () => {
     const navigate = useNavigate();
@@ -44,159 +45,169 @@ const DetailCustomer = () => {
     }, []);
 
     return (
-        <section className="space-y-5 mx-40">
-            <div className="flex flex-wrap justify-between items-center gap-2">
-                <Breadcrumbs className="!font-medium">
-                    <Link
-                        to={config.Routes.listCustomer}
-                        className="font-semibold decoration-primary-700 decoration-1 underline-offset-2 transition hover:underline hover:text-primary-700"
-                    >
-                        <ArrowBackIcon fontSize="small" className="mr-2 mb-1" />
-                        Người dùng
-                    </Link>
-                    <div>{idUser}</div>
-                </Breadcrumbs>
-            </div>
+        <>
+            <section className="space-y-5 max-w-[1240px] m-auto">
+                <div className="flex flex-wrap justify-between items-center gap-2">
+                    <Breadcrumbs className="!font-medium">
+                        <Link
+                            to={config.Routes.listCustomer}
+                            className="font-semibold decoration-primary-700 decoration-1 underline-offset-2 transition hover:underline hover:text-primary-700"
+                        >
+                            <ArrowBackIcon fontSize="small" className="mr-2 mb-1" />
+                            Người dùng
+                        </Link>
+                        <div>{idUser}</div>
+                    </Breadcrumbs>
+                </div>
 
-            <div className="flex flex-col sm:flex-row lg:items-center gap-4 space-y-2 sm:space-y-0">
-                <Image
-                    src={avatar}
-                    alt="avatar"
-                    className="size-40 sm:size-60 rounded shadow object-cover object-center"
-                />
-                <form className="space-y-5 size-full bg-white p-4 rounded-lg">
-                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-                        <Controller
-                            name="username"
-                            control={control}
-                            defaultValue=""
-                            render={({ field }) => (
-                                <TextField
-                                    {...field}
-                                    fullWidth
-                                    label="Tên tài khoản"
-                                    InputProps={{
-                                        readOnly: true,
-                                    }}
-                                />
-                            )}
-                        />
-                        <Controller
-                            name="name"
-                            control={control}
-                            defaultValue=""
-                            render={({ field }) => (
-                                <TextField
-                                    {...field}
-                                    fullWidth
-                                    label="Tên người dùng"
-                                    InputProps={{
-                                        readOnly: true,
-                                    }}
-                                />
-                            )}
-                        />
+                <div className="flex flex-col items-center sm:items-start sm:flex-row  gap-4 space-y-2 sm:space-y-0">
+                    <Avatar
+                        src={avatar}
+                        alt="avatar"
+                        className="!size-36 sm:!size-40 !rounded-lg shadow object-cover object-center bg-white p-1"
+                    />
+                    <form className="space-y-5 size-full bg-white p-5 rounded-lg">
+                        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+                            <Controller
+                                name="username"
+                                control={control}
+                                defaultValue=""
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        fullWidth
+                                        variant="filled"
+                                        label="Tên tài khoản"
+                                        InputProps={{
+                                            readOnly: true,
+                                        }}
+                                    />
+                                )}
+                            />
+                            <Controller
+                                name="name"
+                                control={control}
+                                defaultValue=""
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        fullWidth
+                                        variant="filled"
+                                        label="Tên người dùng"
+                                        InputProps={{
+                                            readOnly: true,
+                                        }}
+                                    />
+                                )}
+                            />
 
-                        <Controller
-                            name="gender"
-                            control={control}
-                            defaultValue=""
-                            render={({ field }) => (
-                                <TextField
-                                    {...field}
-                                    fullWidth
-                                    label="Giới tính"
-                                    InputProps={{
-                                        readOnly: true,
-                                    }}
-                                />
-                            )}
-                        />
-                    </div>
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-                        <Controller
-                            name="email"
-                            control={control}
-                            defaultValue=""
-                            render={({ field }) => (
-                                <TextField
-                                    {...field}
-                                    fullWidth
-                                    label="Email người dùng"
-                                    InputProps={{
-                                        readOnly: true,
-                                    }}
-                                />
-                            )}
-                        />
-                        <Controller
-                            name="phoneNumber"
-                            control={control}
-                            defaultValue=""
-                            render={({ field }) => (
-                                <TextField
-                                    {...field}
-                                    fullWidth
-                                    label="Số điện thoại"
-                                    InputProps={{
-                                        readOnly: true,
-                                    }}
-                                />
-                            )}
-                        />
-                    </div>
-                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-                        <Controller
-                            name="createdDate"
-                            control={control}
-                            defaultValue=""
-                            render={({ field }) => (
-                                <TextField
-                                    {...field}
-                                    fullWidth
-                                    label="Ngày tạo tài khoản"
-                                    InputProps={{
-                                        readOnly: true,
-                                    }}
-                                />
-                            )}
-                        />
+                            <Controller
+                                name="gender"
+                                control={control}
+                                defaultValue=""
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        fullWidth
+                                        variant="filled"
+                                        label="Giới tính"
+                                        InputProps={{
+                                            readOnly: true,
+                                        }}
+                                    />
+                                )}
+                            />
+                        </div>
+                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                            <Controller
+                                name="email"
+                                control={control}
+                                defaultValue=""
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        fullWidth
+                                        variant="filled"
+                                        label="Email người dùng"
+                                        InputProps={{
+                                            readOnly: true,
+                                        }}
+                                    />
+                                )}
+                            />
+                            <Controller
+                                name="phoneNumber"
+                                control={control}
+                                defaultValue=""
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        fullWidth
+                                        variant="filled"
+                                        label="Số điện thoại"
+                                        InputProps={{
+                                            readOnly: true,
+                                        }}
+                                    />
+                                )}
+                            />
+                        </div>
+                        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+                            <Controller
+                                name="createdDate"
+                                control={control}
+                                defaultValue=""
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        fullWidth
+                                        variant="filled"
+                                        label="Ngày tạo tài khoản"
+                                        InputProps={{
+                                            readOnly: true,
+                                        }}
+                                    />
+                                )}
+                            />
 
-                        <Controller
-                            name="locked"
-                            control={control}
-                            defaultValue=""
-                            render={({ field }) => (
-                                <TextField
-                                    {...field}
-                                    fullWidth
-                                    label="Trạng thái tài khoản"
-                                    InputProps={{
-                                        readOnly: true,
-                                    }}
-                                />
-                            )}
-                        />
+                            <Controller
+                                name="locked"
+                                control={control}
+                                defaultValue=""
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        fullWidth
+                                        variant="filled"
+                                        label="Trạng thái tài khoản"
+                                        InputProps={{
+                                            readOnly: true,
+                                        }}
+                                    />
+                                )}
+                            />
 
-                        <Controller
-                            name="isEnabled"
-                            control={control}
-                            defaultValue=""
-                            render={({ field }) => (
-                                <TextField
-                                    {...field}
-                                    fullWidth
-                                    label="Trạng thái kích hoạt"
-                                    InputProps={{
-                                        readOnly: true,
-                                    }}
-                                />
-                            )}
-                        />
-                    </div>
-                </form>
-            </div>
-        </section>
+                            <Controller
+                                name="isEnabled"
+                                control={control}
+                                defaultValue=""
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        fullWidth
+                                        variant="filled"
+                                        label="Trạng thái kích hoạt"
+                                        InputProps={{
+                                            readOnly: true,
+                                        }}
+                                    />
+                                )}
+                            />
+                        </div>
+                    </form>
+                </div>
+            </section>
+        </>
     );
 };
 
